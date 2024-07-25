b23b1946235e7f0a7282ae356e1738e625a2a3b9
Pavan Vemulapalli <pavemu@iu.edu>
2024-07-25 13:20:23 -0400

    lambda function integrated with aws bedrock

---
+++ lambda_function.py
@@ -0,0 +1,34 @@
+import json
+import boto3
+
+bedrock_client = boto3.client('bedrock-runtime')
+
+def lambda_handler(event, context):
+
+    prompt = event['queryStringParameters']['prompt']
+    print(prompt)
+   
+    # Create  Request Syntax - Get details from console & body should be json object - use json.dumps for body
+    bedrock_response = bedrock_client.invoke_model(
+       contentType='application/json',
+       accept='application/json',
+       modelId='cohere.command-light-text-v14',
+       body=json.dumps({
+        "prompt": prompt,
+        "temperature": 0.9,
+        "p": 0.75,
+        "k": 0,
+        "max_tokens": 100}))
+
+    # Convert Streaming Body to Byte(.read method) and then Byte to String using json.loads#
+    bedrock_response_byte = bedrock_response['body'].read()
+    bedrock_response_string = json.loads(bedrock_response_byte)
+
+    # Update the 'return' by changing the 'body'
+    client_final_response = bedrock_response_string['generations'][0]['text']
+    print(client_final_response)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(client_final_response)
+    }
